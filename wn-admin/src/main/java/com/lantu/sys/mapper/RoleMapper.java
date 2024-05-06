3334dc78739336e11dafdfb8226decb02452e69c
妄念 <12789942+herenhui666@user.noreply.gitee.com>
2024-05-06 06:04:38 +0000

    后端
    Signed-off-by: 妄念 <12789942+herenhui666@user.noreply.gitee.com>

---
+++ wn-admin/src/main/java/com/lantu/sys/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package com.lantu.sys.mapper;
+
+import com.lantu.sys.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author laocai
+ * @since 2023-05-05
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
---
+++ wn-admin/src/main/java/com/lantu/sys/service/IRoleService.java
@@ -0,0 +1,16 @@
+package com.lantu.sys.service;
+
+import com.lantu.sys.entity.Role;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author laocai
+ * @since 2023-05-05
+ */
+public interface IRoleService extends IService<Role> {
+
+}
